aede192509a6b1eb317c8511d19f342805be3ae9
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-25 00:49:03 -0700

    Upstart for ubuntu only

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,6 +1,6 @@
 def xvfb_systype
   return 'systemd' if ::File.exist?(Chef.path_to('/proc/1/comm')) &&
                       ::File.open(Chef.path_to('/proc/1/comm')).gets.chomp == 'systemd'
-  return 'upstart' if platform_family?('debian') && ::File.exist?(Chef.path_to('/sbin/initctl'))
+  return 'upstart' if platform?('ubuntu') && ::File.exist?(Chef.path_to('/sbin/initctl'))
   'sysvinit'
 end
