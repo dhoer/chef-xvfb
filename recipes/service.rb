0bfd272b5414cc1d63c58ae26e393e2e41a4820f
Stefano Rivera <stefano@rivera.za.net>
2020-01-27 14:37:43 +0200

    Do not write executeable config files
    systemd and upstart do not need executeable configuration, only sysvinit
    does. systemd complains about this.

--- recipes/service.rb
+++ recipes/service.rb
@@ -2,17 +2,20 @@ case xvfb_systype
 when 'systemd'
   path = '/etc/systemd/system/xvfb.service'
   src = 'systemd.erb'
+  mode = '0644'
 when 'upstart'
   path = '/etc/init/xvfb.conf'
   src = 'upstart.erb'
+  mode = '0644'
 else
   path = '/etc/init.d/xvfb'
   src = 'sysvinit.erb'
+  mode = '0755'
 end
 
 template path do
   source src
-  mode '0755'
+  mode mode
   variables(
     display: node['xvfb']['display'],
     screennum: node['xvfb']['screennum'],
